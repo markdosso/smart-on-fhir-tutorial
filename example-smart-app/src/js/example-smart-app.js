0213fec7ddfee8836c5d6be1cbdbca5e33bebef8
markdosso <71146016+markdosso@users.noreply.github.com>
2022-07-27 08:42:36 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -69,7 +69,7 @@
           var str = '';
           
           allergy.forEach(function(value, index) {
-                str += value.patient.display;          
+                str += value.code.text;          
           });
           
           p.allergy = str;
